7e2e62ebed5ea0bff02e54a564d7fac9c04d541e
LeoBello00 <leovaia32000@gmail.com>
2022-03-15 17:11:31 +0100

    implemented classes

---
+++ src/main/java/model/Card.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Card {
+    private int value;
+    private int movementValue;
+    private int Id;
+
+    public Card(int v, int m, int I) {
+        value = v;
+        movementValue = m;
+        Id = I;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getMovementValue() {
+        return movementValue;
+    }
+
+    public int getId() {
+        return Id;
+    }
+}
--- src/main/java/model/Deck.java
+++ src/main/java/model/Deck.java
@@ -1,4 +1,14 @@
 package model;
 
+enum Wizard{WIZARD1,WIZARD2, WIZARD3,WIZARD4};
+
 public class Deck {
+    private Card cards[];
+    private Card currentCard;
+    public Deck(Card c[]){
+        cards = c;
+        currentCard = null;
+    }
+
+
 }
