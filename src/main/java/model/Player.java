8e086ec511d3329e089530913bcf9511d0af15f9
DavideTonsi <davide.tonsi@mail.polimi.it>
2022-03-15 15:50:51 +0100

    add classes

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Player {
+    private String nickname;
+    private int playerNumber;
+    private Player buddy;
+
+    public String getNickname() {
+        return nickname;
+    }
+}
+
---
+++ src/main/java/model/Professor.java
@@ -0,0 +1,6 @@
+package model;
+
+public class Professor {
+    private boolean isFree;
+    private Player owner;
+}
