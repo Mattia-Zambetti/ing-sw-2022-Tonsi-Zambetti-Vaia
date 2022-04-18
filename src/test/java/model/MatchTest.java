6c020332e2a5bbd86784c9835202a8bd5181af33
DavideTonsi <davide.tonsi@mail.polimi.it>
2022-04-18 22:42:49 +0200

    Merge islands test readded

--- src/test/java/model/MatchTest.java
+++ src/test/java/model/MatchTest.java
@@ -204,6 +204,26 @@ public class MatchTest {
         assertEquals(10,match.previousIsland(0));
     }
 
+    @Test
+    void TestMergeIslands() throws NegativeNumberOfTowerException, InvalidNumberOfTowers, NoListOfSameColoredTowers {
+        Tower tower = new Tower(TowerColor.BLACK,0);
+        ArrayList<Tower> tmpTowers = new ArrayList<Tower>();
+        tmpTowers.add(tower);
+        match.addIslandsTowers(1,tmpTowers);
+        match.setIslandsStudents(0,students);
+        match.setIslandsStudents(1,students);
+        match.mergeIsland(1);
+        for (int i = 0; i < 5; i++)
+            students[i].addAll(students[i]);
+        for (int i = 0; i < 5; i++)
+            assertEquals(students[i].size(),match.getStudentsOnIsland(0)[i].size());
+        assertEquals(1, match.getTowersNumOnIsland(0));
+        match.addIslandsTowers(5,tmpTowers);
+        match.mergeIsland(5);
+        for (int i = 0; i < 5; i++)
+            assertEquals(students[i].size(),match.getStudentsOnIsland(0)[i].size());
+        assertEquals(2, match.getTowersNumOnIsland(0));
+    }
 
     @Test
     void changeTowerColorOnIsland() throws SameInfluenceException, NoTowerException, CardNotFoundException, InvalidNumberOfTowers, NoListOfSameColoredTowers, NegativeNumberOfTowerException, NoIslandException {
