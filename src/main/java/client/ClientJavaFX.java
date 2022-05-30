5df17f74878d5f5661eb77a15a56f9b2101839a5
Mattia Zambetti <mattia.zambetti2000@gmail.com>
2022-05-30 23:33:37 +0200

    First test of GUI with SceneBuilder working

--- src/main/java/client/ClientJavaFX.java
+++ src/main/java/client/ClientJavaFX.java
@@ -6,13 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.URL;
+
 
 public class ClientJavaFX extends Application {
 
     @Override
     public void start(Stage primaryStage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("ClientJavaFX.fxml"));
+            Parent root;
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("ClientJavaFX.fxml"));
+            root = fxmlLoader.load();
+            // = FXMLLoader.load(getClass().getClassLoader().getResource("/ClientJavaFX.fxml"));
             //Group root = new Group();
             Scene scene = new Scene(root, 500, 500);
             //Stage stage = new Stage();
@@ -22,5 +28,22 @@ public class ClientJavaFX extends Application {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        /*String sceneFile = "../src/main/resources/client/ClientJavaFX.fxml";
+        Parent root = null;
+        URL url  = null;
+        try
+        {
+            url  = getClass().getResource( sceneFile );
+            root = FXMLLoader.load( url );
+            System.out.println( "  fxmlResource = " + sceneFile );
+        }
+        catch ( Exception ex )
+        {
+            System.out.println( "Exception on FXMLLoader.load()" );
+            System.out.println( "  * url: " + url );
+            System.out.println( "  * " + ex );
+            System.out.println( "    ----------------------------------------\n" );
+            //throw ex;
+        }*/
     }
 }
