57af7f6b8ea26ce89ee4278c90444991bb0bb047
Mattia <mattia.zambetti2000@gmail.com>
2022-06-07 13:07:40 +0200

    Dashboards, Islands, Cards, FigureCards and Coins added in GameScene.

---
+++ src/main/java/client/ControllerGUIGame.java
@@ -0,0 +1,25 @@
+package client;
+
+import controller.choice.Choice;
+import javafx.fxml.Initializable;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerGUIGame extends ControllerGUIInterface implements Initializable {
+    @Override
+    public void switchScene(Choice choice) throws IOException {
+
+    }
+
+    @Override
+    public void printMessageText(String s) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
