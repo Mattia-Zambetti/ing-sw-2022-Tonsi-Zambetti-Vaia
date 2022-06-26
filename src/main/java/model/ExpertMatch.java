0622bb326f056a6afd37f078baa18ab3707e10ac
DavideTonsi <davide.tonsi@mail.polimi.it>
2022-06-26 20:13:30 +0200

    added expert match comments

--- src/main/java/model/ExpertMatch.java
+++ src/main/java/model/ExpertMatch.java
@@ -76,6 +76,7 @@ public class ExpertMatch extends Match implements ExpertMatchInterface, Serializ
     //OVERRIDED FROM MATCH
 
 
+    /**it contains the additional part to the normal cli in an expert match(figure cards view)*/
     @Override
     public String toString() {
         int i=1;
@@ -108,6 +109,7 @@ public class ExpertMatch extends Match implements ExpertMatchInterface, Serializ
         return res.toString();
     }
 
+    /**It adds to the overrided match method the possibility to reset the figure cards effect every turn*/
     @Override
     public boolean setNextCurrDashboard(){
         centaurEffect=false;
@@ -121,9 +123,9 @@ public class ExpertMatch extends Match implements ExpertMatchInterface, Serializ
         return super.setNextCurrDashboard();
     }
 
+    /**It's an override of a match method that allows to use in the calculation the postman effect*/
     @Override
     public void moveMotherNature(int posizioni) throws FinishedGameIslandException, NoMoreTowerException, NoIslandException, TowerIDAlreadyExistingException, SameInfluenceException, InvalidNumberOfTowers, NoTowerException, NoListOfSameColoredTowers, CardNotFoundException, MaxNumberOfTowerPassedException, NoMoreBlockCardsException, MaxNumberException {
-        //try {
             if (posizioni <= currentPlayerDashboard.getCurrentCard().getMovementValue() + postManValue && posizioni > 0) {
                 int positionTmp = currentIsland;
                 islands.get(positionTmp).setMotherNature(false);
@@ -145,9 +147,6 @@ public class ExpertMatch extends Match implements ExpertMatchInterface, Serializ
             } else throw new MaxNumberException("Cannot move Mother nature that far");
 
             postManValue = 0;
-        /*} catch (Exceptions e){
-            e.manageException(this);
-        }*/
     }
 
 
@@ -155,16 +154,21 @@ public class ExpertMatch extends Match implements ExpertMatchInterface, Serializ
 
     //ONLY EXPERT METHODS:
 
+    /**it's the getter to know if the current player has played the centaur figure card*/
     public boolean isCentaurEffect() {
         return centaurEffect;
     }
 
-
+    /**It allows to set the centaur played from the current player in this turn*/
     public void setCentaurEffect(boolean centaurEffect) {
         this.centaurEffect = centaurEffect;
         Island.setCentaurEffect(centaurEffect);
     }
 
+    /**It allows to play the figure card(param) in this turn if it is in the game. If this figure card is the granny grass, it check
+     * there are others block cards(it sends to the current player an error in this case). In the end it calls the correct play card method
+     * within the figure cards(strategy pattern) and it removes coins from the players and add 1 coin to the price of the chosen figure card.
+     * If the card hasn't been found, it throws CardNotFoundException*/
     public void playFigureCard(FigureCard card) throws CardNotFoundException, FigureCardAlreadyPlayedInThisTurnException, InsufficientCoinException {
         if(figureCards.contains(card)){
             card = figureCards.get(figureCards.indexOf(card));
@@ -184,7 +188,7 @@ public class ExpertMatch extends Match implements ExpertMatchInterface, Serializ
         }else throw new CardNotFoundException("This figure card isn't playable in this match...");
     }
 
-
+    /**It returns the numbre of remaining block cards on the granny grass(0 if there's no granny grass in the match)*/
     public int getRemainingBlockCards() {
         return blockCards;
     }
@@ -231,17 +235,20 @@ public class ExpertMatch extends Match implements ExpertMatchInterface, Serializ
 
     }
 
+    /**It sets the value of the postman for the current player*/
     @Override
     public void setPostManValue() {
         postManValue = 2;
     }
 
+    /**It returns true if the post man value is >0*/
     public boolean isPostManValue(){
         if(postManValue!=0)
             return true;
         return false;
     }
 
+    /**It sets true the knight effect in the current player dashboard, then it sets the old choice phase, and it notifies the players */
     @Override
     public void setIsKnight() {
         currentPlayerDashboard.setKnight(true);
@@ -249,6 +256,7 @@ public class ExpertMatch extends Match implements ExpertMatchInterface, Serializ
         notifyMatchObservers();
     }
 
+    /**It sets true the farmer effect in the current player dashboard, then it sets the old choice phase, and it notifies the players*/
     public void setIsFarmer() {
         currentPlayerDashboard.setFarmerEffect(true);
         setChoicePhase(Controller.getTmpChoice());
@@ -256,7 +264,7 @@ public class ExpertMatch extends Match implements ExpertMatchInterface, Serializ
     }
 
     @Override
-    public Dashboard checkDashboardWithMoreInfluence() throws SameInfluenceException, CardNotFoundException {
+    public Dashboard checkDashboardWithMoreInfluence() throws SameInfluenceException {
         ArrayList<Dashboard> dashboardListTmp = new ArrayList<>(dashboardsCollection);
         int dasboardInfluencer, knightEffect = 0;
         dasboardInfluencer = 0;
@@ -315,6 +323,7 @@ public class ExpertMatch extends Match implements ExpertMatchInterface, Serializ
         return dashboardsCollection.get(dasboardInfluencer);
     }
 
+
     @Override
     public void moveStudentFromEntranceToDR( Student studentToBeMoved ) throws NoMasterException, WrongColorException {
         Student tmpStudent;
@@ -339,10 +348,12 @@ public class ExpertMatch extends Match implements ExpertMatchInterface, Serializ
         notifyMatchObservers();
     }
 
+    /**It returns the figure cards in the game */
     public List<FigureCard> showFigureCardsInGame(){
         return new ArrayList<>(figureCards);
     }
 
+    /**A method used to send to the players the new match with the request to do a choice for the chosen figure card*/
     public void notifyFigureCard(FigureCard figureCard){
         choicePhase = figureCard.getActualChoice();
         setChanged();
@@ -352,6 +363,9 @@ public class ExpertMatch extends Match implements ExpertMatchInterface, Serializ
 
     //There's three overloading methods to manage figure with students different operations:
 
+    /**It's the first overload of this method (for the merchant figure card): it takes as param a set of chosen students(only one for the merchant)
+     * and the chosen island; it takes the student from the merchant and add it to the chosen island;
+     * in the end it sets the old choice and notifies the clients*/
     public void takeStudentsOnFigureCard(Set<Student> chosenStudents, int islandPosition) throws MaxNumberException, InexistentStudentException, StudentIDAlreadyExistingException, WrongColorException, NoMoreStudentsException, NoIslandException {
         for (FigureCard f : figureCards) {
             if (f instanceof Merchant) {
@@ -364,6 +378,10 @@ public class ExpertMatch extends Match implements ExpertMatchInterface, Serializ
         notifyMatchObservers();
     }
 
+    /**It's the second overload of this method (for the jester figure card): it takes as param a set of chosen students(from one to three for the jester)
+     * and a set of students from the entrance; it checks if the params has the same size and if the studentsFromEntrance are in the entrance
+     * (in case it throws MaxNumberException). Then it removes the studentsFromEntrance from the entrance and it puts the chosenStudents
+     * in it. In the end it refills the jester figure card with the studentsFromEntrance and it sets the old choice notifying the clients */
     public void takeStudentsOnFigureCard(Set<Student> chosenStudents, Set<Student> studentsFromEntrance)
             throws MaxNumberException, InexistentStudentException,
             StudentIDAlreadyExistingException, NoMoreStudentsException {
@@ -414,6 +432,9 @@ public class ExpertMatch extends Match implements ExpertMatchInterface, Serializ
         notifyMatchObservers();
     }
 
+    /**It's used when a players choose the granny grass figure card; it takes as param the island position to block and sets it forbidden,
+     * then it removes a block card(blockCard decrementing) and in the end it sets the old choice and notifies the clients.
+     * It throws NoIslandException if the islandToSetForbidden doesn't exist*/
     public void placeForbiddenCards(int islandToSetForbidden) throws NoIslandException, NoMoreBlockCardsException {
         if(islandPositions.contains((Integer) islandToSetForbidden)){
             islands.get(islandToSetForbidden).setForbidden(true);
@@ -435,7 +456,8 @@ public class ExpertMatch extends Match implements ExpertMatchInterface, Serializ
         notifyMatchObservers();
     }
 
-
+    /**It's used when a players choose the mushroom collector figure card; it locks the chosen
+     * color by setting his boolean and in the end it sets the old choice and notifies the clients*/
     public void blockColorForInfluence(Color color){
         colorBlocked = color.ordinal();
         color.lockColor();
